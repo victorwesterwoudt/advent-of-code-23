20cda3f06b358ca9f56fd960c4e73bf5ced8cc54
Victor Westerwoudt <victor.westerwoudt@clockworks.co>
2023-12-02 11:30:02 +0100

    [AOC-23] Day 2 - added some docs and reformatting

--- day_1.py
+++ day_1.py
@@ -17,22 +17,68 @@ class Digits(Enum):
 
 
 class Day1(Day):
+    """
+    Class representing the solution for Day 1 of the Advent of Code challenge.
+    """
+
     @property
     def data(self) -> list:
+        """
+        Get the raw data for the challenge.
+
+        Returns:
+            list: The raw data.
+        """
         return self.raw_data
 
     @staticmethod
     def _filter_data(data: list) -> list:
+        """
+        Filter the data by removing non-digit characters.
+
+        Args:
+            data (list): The data to filter.
+
+        Returns:
+            list: The filtered data.
+        """
         return [re.sub(r"\D", "", line) for line in data]
 
     @classmethod
     def _numerize(cls, data: list) -> list:
+        """
+        Convert the filtered data into a list of numbers.
+
+        Args:
+            data (list): The filtered data.
+
+        Returns:
+            list: The numerized data.
+        """
         return [int(x[0]) * 10 + int(x[-1]) for x in cls._filter_data(data)]
 
     def part_1(self) -> int:
+        """
+        Solve part 1 of the challenge.
+
+        Returns:
+            int: The solution for part 1.
+        """
         return sum(self._numerize(self.data))
 
     def part_2(self) -> int:
+        """
+        Solve part 2 of the challenge.
+
+        The trick here is that the strings of digits that are being replaced by numbers
+        can have their characters overlap with the string of another digit. For example,
+        "eightwo" should be replaced with "82". This is why replacing the whole world 
+        with a number doesn't work. Instead, we need to replace the middle characters of the 
+        number and leave the first and last characters intact.
+
+        Returns:
+            int: The solution for part 2.
+        """
         temp = self.data
         for digit in Digits:
             temp = [
--- day_2.py
+++ day_2.py
@@ -1,19 +1,52 @@
 from src import Day
 from functools import reduce
+from enum import Enum
+
+
+class RGB(Enum):
+    RED = (0, "red")
+    GREEN = (1, "green")
+    BLUE = (2, "blue")
+
+    @property
+    def color(self) -> str:
+        """Get the color string."""
+        return self.value[1]
+
+    @property
+    def index(self) -> int:
+        """Get the index."""
+        return str(self.value[0])
 
 
 class Day2(Day):
+    """Class representing Day 2 of the Advent of Code challenge."""
+
     @property
     def data(self) -> dict:
+        """Process the raw data and return a dictionary of game data.
+
+        Returns:
+            dict: A dictionary containing game data.
+                  The keys are game numbers and the values are 3D
+                  lists representing the moves made in each game.
+        """
         games = {}
         for line in self.raw_data:
-            line = line.replace("red", "0").replace("blue", "2").replace("green", "1")
+            line = (
+                line.replace(RGB.RED.color, RGB.RED.index)
+                .replace(RGB.GREEN.color, RGB.GREEN.index)
+                .replace(RGB.BLUE.color, RGB.BLUE.index)
+            )
             game, sets = line.split(":")
             game = int(game.strip().replace("Game ", ""))
             sets = sets.split(";")
             game_data = [[0] * len(sets) for _ in range(3)]
             for i, s in enumerate(sets):
-                moves = [tuple(move.strip().split(" ")) for move in s.strip().split(",")]
+                moves = [
+                    tuple(move.strip().split(" "))
+                    for move in s.strip().split(",")
+                ]
                 for move in moves:
                     game_data[int(move[1])][i] += int(move[0])
             games[game] = game_data
@@ -21,9 +54,29 @@ class Day2(Day):
 
     @staticmethod
     def _check_moves(draws: list[int], maximum: int) -> bool:
+        """Check if the draws in a game are within the maximum limits.
+
+        Args:
+            draws (list[int]): The draws made in a game.
+            maximum (int): The maximum limit for a draw.
+
+        Returns:
+            bool: True if all draws are within the maximum limit, False otherwise.
+        """
         return not any(draw > maximum for draw in draws)
 
     def _check_games(self, maximum: list[int, int, int]) -> dict:
+        """Check the games and return a dictionary of valid games.
+
+        Args:
+            maximum (list[int, int, int]): The maximum limits for each draw.
+
+        Returns:
+            dict: A dictionary containing valid games.
+                  The keys are game numbers and the values is a list with a
+                  list for each cube color representing the moves made in each
+                  game for that color.
+        """
         return dict(
             filter(
                 lambda item: all(
@@ -34,12 +87,37 @@ class Day2(Day):
         )
 
     def part_1(self, maximum: list[int, int, int]) -> int:
+        """Calculate the sum of game numbers for valid games.
+
+        Args:
+            maximum (list[int, int, int]): The maximum limits for each draw.
+
+        Returns:
+            int: The sum of game numbers for valid games.
+        """
         return sum(self._check_games(maximum).keys())
 
+    def _mvg(self, game: list[list[int]]) -> list[int]:
+        """Calculate the Minimum Viable Game (MVG)
+        Calculates the minimum cubes needed for each color to
+          have a valid game.
+        Args:
+            game (list[list[int]]): The moves made in a game.
+
+        Returns:
+            list[int]: The minimum cubes needed to have a valid game.
+        """
+        return [max(x) for x in game]
+
     def part_2(self) -> int:
+        """Calculates the sum of MVG products for all games.
+
+        Returns:
+            int: The product of the maximum draws in each game.
+        """
         return sum(
             [
-                reduce(lambda x, y: x * y, [max(s) for s in sets])
+                reduce(lambda x, y: x * y, self._mvg(sets))
                 for _, sets in self.data.items()
             ]
         )
