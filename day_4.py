9b8c2ff124c21f83cc18f52e07fcb4db42c8ee86
Victor Westerwoudt <victor.westerwoudt@clockworks.co>
2023-12-06 09:00:51 +0100

    [AOC-23] formatting

--- day_4.py
+++ day_4.py
@@ -4,36 +4,39 @@ from src import Day
 
 
 class Day4(Day):
-    
     @cached_property
     def data(self):
         data = defaultdict()
 
         for line in self.raw_data:
-            c, line = line.split(':')
+            c, line = line.split(":")
             card = int(c.split()[-1])
-            nums, wins = line.split('|')
+            nums, wins = line.split("|")
             nums = [int(n.strip()) for n in nums.split()]
             wins = [int(w.strip()) for w in wins.split()]
 
             data[card] = list(set(nums) & set(wins))
-    
-        return data    
+
+        return data
 
     def part_1(self):
-        return sum([2**(len(card)-1) for _, card in self.data.items() if len(card) > 0])
-    
+        return sum(
+            [
+                2 ** (len(card) - 1)
+                for _, card in self.data.items()
+                if len(card) > 0
+            ]
+        )
 
     def part_2(self):
         copies = defaultdict(int)
         for card, wins in self.data.items():
             copies[card] += 1
             for j in range(len(wins)):
-                if card+j+1 in self.data:
-                    copies[card+j+1] += copies[card]
+                if card + j + 1 in self.data:
+                    copies[card + j + 1] += copies[card]
 
         return sum(copies.values())
-
 
 
 if __name__ == "__main__":
--- day_6.py
+++ day_6.py
@@ -5,30 +5,32 @@ import math
 class Day6(Day):
     @property
     def data1(self):
-        times = [int(x) for x in self.raw_data[0].split(':')[1].split()]
-        distances = [int(x) for x in self.raw_data[1].split(':')[1].split()]
+        times = [int(x) for x in self.raw_data[0].split(":")[1].split()]
+        distances = [int(x) for x in self.raw_data[1].split(":")[1].split()]
 
         return times, distances
-    
+
     @property
     def data2(self):
-        return [int(''.join(map(str,x))) for x in self.data1]
+        return [int("".join(map(str, x))) for x in self.data1]
 
     @staticmethod
     def solve(t, d):
-        sols = sorted([
-            (-t + (t**2 - 4*d)**0.5) / 2,
-            (-t - (t**2 - 4*d)**0.5) / 2
-        ])
+        sols = sorted(
+            [
+                (-t + (t**2 - 4 * d) ** 0.5) / 2,
+                (-t - (t**2 - 4 * d) ** 0.5) / 2,
+            ]
+        )
 
         sols = math.ceil(sols[0]), math.floor(sols[1])
 
         return sols[1] - sols[0] + 1
-    
+
     def part_1(self):
         opts = 1
-        for t,d in zip(*self.data1):
-            opts *= self.solve(t,d)
+        for t, d in zip(*self.data1):
+            opts *= self.solve(t, d)
         return opts
 
     def part_2(self):
